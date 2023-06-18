c27dc6b7f15dd8788c5d38a49717fa7cefe5b087
Henrique Brendon <henriquebrendonti26@gmail.com>
2023-06-18 16:33:56 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,18 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position =  position;
 	}
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) ==  null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position =  null;
+		pieces[position.getRow()][position.getColumn()] = null ;
+		return aux;
+	}
 	
 	// Defensive programming methods
 	private boolean positionExists(int row, int column) {
@@ -58,6 +70,8 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 	
 	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -21,6 +23,24 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	public void validateSourcePosition(Position position) {
+		if(!board.thereIsApiece(position)) {
+			throw new ChessException("There is not piece on source position");
+		}
+	}
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
